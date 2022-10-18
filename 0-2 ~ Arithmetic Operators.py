2d2c843576b3de5df80d33e6e15927a2c8f251c6
Mte <wmetehan@gmail.com>
2022-10-18 03:36:00 +0300

    Python

---
+++ 0-2 ~ Arithmetic Operators.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    sum = a + b
+    diff = abs(a - b)
+    product = a * b
+    
+    print(sum)
+    print(diff)
+    print(product)
---
+++ 0-3 ~ Python Division.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    result = a / b
+    print(int(result))
+    print(float(result))
